3ac2b836ef1808fe4103483114327f477aae70f3
Chamod_Kalupahana <chamodkalupahana@gmail.com>
2023-11-23 05:23:07 +0000

    5. Added __gt__ method

--- person_class.py
+++ person_class.py
@@ -1,3 +1,7 @@
+# Import modules
+
+import datetime
+
 # Create a person class
 
 class Person():
@@ -8,9 +12,17 @@ class Person():
 
     def __str__(self):
         return 'Name: ' + self.name + ', Date of Birth: ' +  str(self.date_of_birth) + ', Num. of friends: ' + str(len(self.friends))
+    
+    def __gt__(self, other_person):
+        date_self = datetime.datetime.strptime(self.date_of_birth, '%Y-%m-%d')
+        date_other_person = datetime.datetime.strptime(other_person.date_of_birth, '%Y-%m-%d')
+
+        # for datetime objects, being > means coming later
+        return date_other_person > date_self
 
 
-# Test if input_list is correct
-test_person = Person('Cham', '2001-05-30', friends=['Jas', 'Caitlin', 'Dylan'])
+# Test if > is correct
+Cham = Person('Cham', '2001-05-30', friends=['Jas', 'Caitlin', 'Dylan'])
+Jas = Person('Jas', '2003-01-21', friends=['Cham', 'Caitlin'])
 
-print(test_person)
+print(Cham > Jas)
