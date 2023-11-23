565d7f4b8ca0c12cd36cd7de564fb465938ba94e
Chamod_Kalupahana <chamodkalupahana@gmail.com>
2023-11-23 05:34:10 +0000

    Added add_function

--- person_class.py
+++ person_class.py
@@ -20,9 +20,19 @@ class Person():
         # for datetime objects, being > means coming later
         return date_other_person > date_self
 
+    def add_friend(self, other_person):
+        self.friends.append(other_person.name)
+        other_person.friends.append(self.name)
+        pass
 
-# Test if > is correct
-Cham = Person('Cham', '2001-05-30', friends=['Jas', 'Caitlin', 'Dylan'])
-Jas = Person('Jas', '2003-01-21', friends=['Cham', 'Caitlin'])
+# Test if add_friend is correct
+Cham = Person('Cham', '2001-05-30', friends=['Ben', 'Caitlin', 'Dylan'])
+Jas = Person('Jas', '2003-01-21', friends=['Charles', 'Caitlin'])
 
-print(Cham > Jas)
+print(Cham.friends)
+
+Cham.add_friend(Jas)
+
+print(Cham.friends)
+print(Jas.friends)
+
