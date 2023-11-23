aca65fd85ecd492392e8fb613442493127909275
Chamod_Kalupahana <chamodkalupahana@gmail.com>
2023-11-23 05:08:23 +0000

    4. Defined __str__ method

--- person_class.py
+++ person_class.py
@@ -1,22 +1,16 @@
 # Create a person class
 
 class Person():
-    def __init__(self, name, date_of_birth, input_list=None):
+    def __init__(self, name, date_of_birth, friends=None):
         self.name = name
         self.date_of_birth = date_of_birth
-        self.list = input_list if input_list else []
+        self.friends = friends if friends else []
 
-# Test if init is correct
+    def __str__(self):
+        return 'Name: ' + self.name + ', Date of Birth: ' +  str(self.date_of_birth) + ', Num. of friends: ' + str(len(self.friends))
 
-test_person = Person('Cham', 4)
-
-print(test_person.name)
-print(test_person.date_of_birth)
-print(test_person.list)
 
 # Test if input_list is correct
+test_person = Person('Cham', '2001-05-30', friends=['Jas', 'Caitlin', 'Dylan'])
 
-
-test_person = Person('Cham', 4, input_list=[1, 2, 3])
-
-print(test_person.list)
+print(test_person)
